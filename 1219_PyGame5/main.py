ceab404ca020c1b10efe341a7a7feb99a1aad7ed
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2024-12-19 18:32:21 +0300

    Добавили рисунки бомб по нажатию клавиши мыши

--- 1219_PyGame5/main.py
+++ 1219_PyGame5/main.py
@@ -1,4 +1,5 @@
-# Example file showing a basic pygame "game loop"
+import os
+import sys
 import pygame
 
 
@@ -9,17 +10,28 @@ def main():
     screen = pygame.display.set_mode(size)
     screen.fill("purple")
     clock = pygame.time.Clock()
+    image = load_image('bomb.png')
     running = True
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                screen.blit(image, event.pos)
         # RENDER YOUR GAME HERE
 
         pygame.display.flip()
         clock.tick(60)  # limits FPS to 60
     pygame.quit()
+
+def load_image(name):
+    fullname = os.path.join('images', name)
+    if not os.path.isfile(fullname):
+        print(f'Файл с изображением {fullname} не найден')
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
 
 if __name__ == '__main__':
     main()
