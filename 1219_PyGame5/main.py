9c83eaa64faa06284df8243619fbc9a7b45833be
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2024-12-19 18:24:56 +0300

    Создали шаблон из документации версия 2

--- 1219_PyGame5/main.py
+++ 1219_PyGame5/main.py
@@ -1,28 +1,25 @@
 # Example file showing a basic pygame "game loop"
 import pygame
 
-# pygame setup
-pygame.init()
-size = width, height = (1280, 720)
-screen = pygame.display.set_mode(size)
-clock = pygame.time.Clock()
-running = True
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # fill the screen with a color to wipe away anything from last frame
+def main():
+    # pygame setup
+    pygame.init()
+    size = width, height = (1280, 720)
+    screen = pygame.display.set_mode(size)
     screen.fill("purple")
+    clock = pygame.time.Clock()
+    running = True
 
-    # RENDER YOUR GAME HERE
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        # RENDER YOUR GAME HERE
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+        pygame.display.flip()
+        clock.tick(60)  # limits FPS to 60
+    pygame.quit()
 
-    clock.tick(60)  # limits FPS to 60
-
-pygame.quit()
+if __name__ == '__main__':
+    main()
