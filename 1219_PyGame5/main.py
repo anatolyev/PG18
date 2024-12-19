ffe970761f7f894100c36a242279ba6103e61492
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2024-12-19 18:43:34 +0300

    Прозрачность Совы

--- 1219_PyGame5/main.py
+++ 1219_PyGame5/main.py
@@ -10,7 +10,7 @@ def main():
     screen = pygame.display.set_mode(size)
     screen.fill("purple")
     clock = pygame.time.Clock()
-    image = load_image('bomb.png')
+    image = load_image('owls.png', -1)
     running = True
 
     while running:
@@ -25,12 +25,19 @@ def main():
         clock.tick(60)  # limits FPS to 60
     pygame.quit()
 
-def load_image(name):
+def load_image(name, colorkey=None):
     fullname = os.path.join('images', name)
     if not os.path.isfile(fullname):
         print(f'Файл с изображением {fullname} не найден')
         sys.exit()
     image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()  # создаёт новую копию изображения с таким же форматом пикселей, как у экрана
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()  # метод для преобразования изображения с сохранением информации о прозрачности
     return image
 
 if __name__ == '__main__':
