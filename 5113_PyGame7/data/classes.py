ee975955c25a70aea33aa1b951bcfc73ddfe74e1
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-01-13 19:11:10 +0300

    Добавили новый класс для игрока

--- 5113_PyGame7/data/classes.py
+++ 5113_PyGame7/data/classes.py
@@ -9,3 +9,11 @@ class Tile(pygame.sprite.Sprite):
         self.image = tile_images[tile_type]
         self.rect = self.image.get_rect().move(
             tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, player_image, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
--- 5113_PyGame7/data/functions.py
+++ 5113_PyGame7/data/functions.py
@@ -39,17 +39,20 @@ def generate_level(level):
     tile_images = {
         'wall': load_image('box.png'),
         'empty': load_image('grass.png'),
-        'player': load_image('mar.png'),
     }
+
+    player_image = load_image('mar.png')
 
     for y in range(len(level)):
         for x in range(len(level[y])):
             match level[y][x]:
                 case '.': Tile(tile_images, 'empty', x, y)
                 case '#': Tile(tile_images, 'wall', x, y)
-                case "@": Tile(tile_images, 'player', x, y)
+                case "@":
+                    Tile(tile_images, 'empty', x, y)
+                    new_player = Player(player_image, x, y)
 
-    return None
+    return new_player, x, y
 
 
 def terminate():
@@ -103,7 +106,7 @@ def load_level(filename):
 def game_cycle(user_name, difficulty):
     """Главный игровой цикл"""
 
-    generate_level(load_level(LEVELS_LIST[difficulty]))
+    player, x, y = generate_level(load_level(LEVELS_LIST[difficulty]))
 
     running = True
     while running:
@@ -114,6 +117,7 @@ def game_cycle(user_name, difficulty):
 
         screen.fill(pygame.Color(0, 0, 0))
         tiles_group.draw(screen)
+        player_group.draw(screen)
 
         pygame.display.flip()
         clock.tick(FPS)
